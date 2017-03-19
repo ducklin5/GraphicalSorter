cdb62fbeaa4ad81c0d3b2922de90e963bf9d74f6
ducklin5 <blue-boy-cool@hotmail.com>
2017-03-19 19:57:10 +0300

    Using Vector isntead of ArraynSize

--- src/Sort.cpp
+++ src/Sort.cpp
@@ -3,125 +3,137 @@
 #include <time.h>
 
 //create a new type
-struct ArraynSize{
-	int size;
-	float* set;
-};
 
 //declare our function names
-void printArray( struct ArraynSize );
-void swap(float& x, float& y);
-void bubbleSort (struct ArraynSize );
-void drawLines (struct ArraynSize);
-void bubble_sort( struct ArraynSize p);
+void printArray(std::vector<float> p);
+void drawLines (std::vector<float> p);
+void bubbleSort (std::vector<float>& p);
+void bubble_sort(std::vector<float>& p);
 
 int main()
 {
 
-	//Create an ArraynSize object
-	//which holds the array of random numbers and the size of said array
-	//this is needed to use sf::Thread for multi-threading
-	struct ArraynSize Param;
-	Param.size = 200;
-	float a[Param.size];
-	Param.set = a;
-	//create an array with random numbers
+	//create our array with 500 elements
+	std::vector<float> array(20);
+
+	//fill the array with random numbers from 0 to 299
 	int seed = time(NULL);
 	srand(seed);
-	for(int i = 0; i < Param.size; i++){
-		Param.set[i] = rand()%300;
+	int n = array.size();
+	for(int i = 0; i < n; i++){
+		array[i] = rand()%300;
 	};
 
-	struct ArraynSize Param_2 = Param;
+	//print our array for clarity
+	for( int i=0; i< n; ++i){
+	  std::cout << array[i] << ' ';
+	}
+	std::cout << " : Array created" << std::endl;
 
-	// Draw our array of random numbers as lines
-	sf::Thread tDrawLines(&drawLines,Param);
-	sf::Thread tSort(&bubble_sort,Param);
-
-	tDrawLines.launch();
-	sf::sleep(sf::seconds(2));
 	// Sort our Array of random numbers
+	sf::Thread tSort(&bubbleSort, array);
 	tSort.launch();
+	sf::sleep(sf::seconds(2));
+	// Draw our array of random numbers as lines
+	sf::Thread tDrawLines(&drawLines, array);
+	tDrawLines.launch();
+
 }
 
 //Draw any array of floating numbers as lines
-void drawLines (struct ArraynSize p){
+void drawLines (std::vector<float> p){
 	//create a window called Sort Algorithms
 	//we will draw to this
 	sf::RenderWindow window(sf::VideoMode(600, 600), "Sort Algorithms");
 
-	sf::Vector2f Center(1000, 1000);
+	//print the size of the input array for clarity
+	int n = p.size();
+	std::cout << n << std::endl;
+
 	//make an array of rectangle with the same number of elements as the input array
-	sf::RectangleShape rectangle[p.size];
-	for (int x = 0; x < p.size; x++ )
-		rectangle[x].setPosition(1, 1 + x);
+	sf::RectangleShape rectangle[n];
+
+	std::cout << n << std::endl;
+
+	//set the starting position of each line
+
+	for (int x = 0; x < n; x++ ){
+		rectangle[x].setPosition(1, 1 + 3*x);
+		std::cout << n << std::endl;
+	}
+
 
 	while (window.isOpen())
 	{
+		//clear window
 		window.clear();
-		for (int x = 0; x < p.size; x++ ){
-			if (rectangle[x].getSize() != sf::Vector2f(p.set[x], 1)){
-				rectangle[x].setSize(sf::Vector2f(p.set[x], 1));
-				rectangle[x].setFillColor(sf::Color::Red);
-			}else if ( rectangle[x].getFillColor() != sf::Color::White ){
+		//update the size of each line by checking the input array elemts
+		for (int x = 0; x < n; x++ ){
+			//if (rectangle[x].getSize() != sf::Vector2f(p[x], 1)){
+				//std::cout << p[x] << ", " ;
+				rectangle[x].setSize(sf::Vector2f(p[x], 1));
+				//rectangle[x].setFillColor(sf::Color::Red);
+			//}else if ( rectangle[x].getFillColor() != sf::Color::White ){
 				rectangle[x].setFillColor(sf::Color::White);
-			}
+			//}
+			//redraw each triangle
 			window.draw(rectangle[x]);
 		}
+		//std::cout << std::endl ;
 		window.display();
 
 		sf::Event event;
-		while (window.pollEvent(event))
+		while (window.pollEvent(event)){
 			if (event.type == sf::Event::Closed)
 				window.close();
+		}
 	}
 }
 
 
 // Print any array of floating numbers
-void printArray( struct ArraynSize p )
-{
-	for (int i = 0; i < p.size - 1; i++) {
-		std::cout << p.set[i] << ", ";
+void printArray( std::vector<float> p ){
+	int n = p.size();
+	for (int i = 0; i < n - 1; i++) {
+		std::cout << p[i] << ", ";
 	}
-	std::cout << p.set[p.size-1] << std::endl;
+	std::cout << p[p.size()-1] << std::endl;
 }
 
 
-// Create sort function (with assisting swap function) >> not my code
-void swap(float& x, float& y)
-{
-	float temp = x;
-	x = y;
-	y = temp;
-}
+// Create sort function >> not my code
+//I added timers to each algorithm
 
-void bubbleSort( struct ArraynSize p)
+void bubbleSort( std::vector<float> & p)
 {
 	sf::Clock clock;
-	for (int i = p.size-1; i > 0; i--){
+	int n = p.size();
+	for (int i = n-1; i > 0; i--){
 		for (int j = 0; j < i; j++){
 			//printArray( p );
-			if (p.set[j] > p.set[j+1]){swap(p.set[j],p.set[j+1]);};
+			if (p[j] > p[j+1]){std::swap(p[j],p[j+1]);};
 		}
 	}
+	printArray( p );
 	sf::Time elps = clock.getElapsedTime();
 	std::cout << "It took " << elps.asMilliseconds() << " msecs to sort." <<  std::endl;
+
 }
 
-void bubble_sort( struct ArraynSize p ) {
+void bubble_sort( std::vector<float> & p ) {
 	sf::Clock clock;
-	for(int k = 0; k< p.size-1; k++) {
-          // (p.size-k-1) is for ignoring comparisons of elements which have already been compared in earlier iterations
+	int n = p.size();
+	for(int k = 0; k< n-1; k++) {
+		// (p.size-k-1) is for ignoring comparisons of elements which have already been compared in earlier iterations
 
-          for(int i = 0; i < p.size-k-1; i++) {
-        	  printArray( p );
-                 if(p.set[ i ] > p.set[ i+1] ) {
-                      // here swapping of positions is being done.
-                	 swap(p.set[i],p.set[i+1]);
-                  }
-          }
-    }
+		for(int i = 0; i < n-k-1; i++) {
+			//printArray( p );
+			if(p[ i ] > p[ i+1] ) {
+				// here swapping of positions is being done.
+				std::swap(p[i],p[i+1]);
+			}
+		}
+	}
 	sf::Time elps = clock.getElapsedTime();
 	std::cout << "It took " << elps.asMicroseconds() << " usecs to sort." <<  std::endl;
 }
